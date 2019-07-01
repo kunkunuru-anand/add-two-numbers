5a3cf2ee26aff189028c41f8901f3d0b8179259d
kunkunuru-anand <48170709+kunkunuru-anand@users.noreply.github.com>
2019-07-01 16:03:49 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+  public static void main(String [] args)
+   {
+     int a,b,c;
+     Scanner input = new Scanner(system.in);
+     a=input.nextInt();
+     b=input.nextInt();
+     c=a+b;
+     system.out.println(" the result is "+c);
+     }
+ }    
